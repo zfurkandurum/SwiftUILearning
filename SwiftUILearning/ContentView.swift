3aeaae3a8c91c1c9231c907dee1731a518ce4780
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2023-09-30 21:49:37 +0300

    Initial Commit

---
+++ SwiftUILearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILearning
+//
+//  Created by Zeki Furkan Durum on 30.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Zeki Furkan Durum on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
